bc69fe3426b5882a1fe9b9ef1ac7b9757aba9dbf
ameWex <bilousvladw@gmail.com>
2023-11-22 12:59:16 +0100

    init

--- src/route/calc.js
+++ src/route/calc.js
@@ -5,11 +5,21 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
-const calc = require('./calc')
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
-router.use('/calc', calc)
+router.get('/', function (req, res) {
+    res.render('calc', {
+        name: 'calc',
+
+        component: [],
+
+        title: 'Calc',
+
+        data: {
+
+        },
+    })
+})
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
